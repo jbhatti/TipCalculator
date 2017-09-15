23a3988a9f8a8add1a8ed59ba2baa371a0c33ff7
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-15 11:43:02 -0400

    first commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Jaison Bhatti on 2017-09-15.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCalculator/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  TipCalculator
+//
+//  Created by Jaison Bhatti on 2017-09-15.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
